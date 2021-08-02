52ad4e305dbfc4677b9a479a41c187ea017f895a
hfpaglione <87154263+hfpaglione@users.noreply.github.com>
2021-08-01 21:43:26 -0300

    Add files via upload

--- hola_mundo.py
+++ hola_mundo.py
@@ -1,1 +1,9 @@
-print("Hola Mundo! mi nombre es Hector")
+battery_level = 10
+battery_low = False
+low_power_mode = False
+wifi_disconnect = False
+lock_screen = False
+if battery_level < 15:
+    battery_low = True
+if (wifi_disconnect == True) and (lock_screen == False):
+    print("El dispositivo se ha desconectado de la red")
